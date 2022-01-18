2b737b1a67abcf7a2f3f86ea09ff62ff4a4ada79
Dudhat_Darshan <dudhatdarshan1@gmail.com>
2022-01-18 20:28:41 +0530

    18/1/2022

---
+++ project1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class project1
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.printf("Hello world");
+		int a=sc.nextInt();
+	}
+}
